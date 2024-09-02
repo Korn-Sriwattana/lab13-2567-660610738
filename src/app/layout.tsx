9cd94b7d304b53a228927a1f7f92945eceee7db5
Korn-Sriwattana <korn_s@cmu.ac.th>
2024-09-02 18:25:07 +0700

    update klai done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,9 +34,9 @@ export default function RootLayout({
           <Navbar />
           {children}
           <Footer
-            year="2023"
-            fullName="Chayanin Suatap"
-            studentId="650610560"
+            studentId = "660610738"
+            year = "2024"
+            fullName = "Korn Sriwattana"
           />
         </div>
       </body>
